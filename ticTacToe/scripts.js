17a5c7fa1dc08c3179137d8f0d316c26d33ebf4b
tiagoeufsantos <tiago.euf.santos@gmail.com>
2025-05-10 02:08:02 +0100

    TicTacToe game base created

---
+++ ticTacToe/scripts.js
@@ -0,0 +1,13 @@
+let currentPlayer = 1;
+/*
+changeImage = () => {
+  const img = document.getElementById("xo1");
+  if (img.src.includes("")) {
+    img.src = "/ticTacToe/resources/circle.png";
+  } else {
+    img.src = "/ticTacToe/resources/cross.png";
+  }
+};
+
+onclick.changeImage;
+*/
